96e57d197715b388967e4c6d606c4122f3567476
yashwiai <yash@wadhwaniai.org>
2023-02-15 16:56:40 +0530

    change description

--- app.py
+++ app.py
@@ -81,15 +81,8 @@ def process_image(image, text_prompt, threhsold, visual_prompt=None):
 
 if __name__ == "__main__":
 
-    title = "Interactive demo: zero-shot image segmentation with CLIPSeg"
-    description = "Demo for using CLIPSeg, a CLIP-based model for zero- and one-shot image segmentation. To use it, simply upload an image and add a text to mask (identify in the image), or use one of the examples below and click 'submit'. Results will show up in a few seconds."
-    article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2112.10003'>CLIPSeg: Image Segmentation Using Text and Image Prompts</a> | <a href='https://huggingface.co/docs/transformers/main/en/model_doc/clipseg'>HuggingFace docs</a></p>"
-
-
     with gr.Blocks() as demo:
-        gr.Markdown("# CLIPSeg: Image Segmentation Using Text and Image Prompts")
-        gr.Markdown(article)
-        gr.Markdown(description)
+        gr.Markdown("# Image Segmentation Using Text and Image Prompts")
 
         with gr.Row():
             with gr.Column():
